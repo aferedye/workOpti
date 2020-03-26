b11dfa5caa13d043be22cd3f42f1ab369fee5748
Feredye Antoine <aferedye@gmail.com>
2020-03-26 21:13:25 +0100

    Update SecurityController
    Ajout de la fonction login/logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,16 +12,11 @@ use App\Entity\User;
 use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="login")
-     */
-    public function login()
-    {
-        return $this->render('security/login.html.twig');
-    }
+    
 
     /**
      * @Route("/register", name="register")
@@ -64,6 +59,31 @@ class SecurityController extends AbstractController
             'error' => $error,
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
 }
