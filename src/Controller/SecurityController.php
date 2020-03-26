3d4642f65a48255156da7173b801846039fccae7
Feredye Antoine <aferedye@gmail.com>
2020-03-26 15:36:25 +0100

    New function REGISTER
    Ajout de la fonction : définition des rôles, hachage mdp, création formulaire, ajout en BDD

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -17,10 +25,45 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/register", name="register")
+     * @return Response
+     * @throws Exception
+     * @param UserPasswordEncoderInterface $encoder
      */
-    public function register()
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
-        return $this->render('security/register.html.twig');
+        $error = null;
+        $roles = ["ROLE_USER"];
+        $user = new User();
+        $user->setRoles($roles);
+
+        $form = $this->createFormBuilder($user)
+            ->add('email', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add("submit", SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->render('accueil/index.html.twig', [
+                'user' => $this->getUser()
+            ]);
+        };
+
+        return $this->render('security/register.html.twig', [
+            'error' => $error,
+            'form' => $form->createView()
+        ]);
     }
 
 }
