eebbab87fd84234df6df8a57a410e79df7246b08
Feredye Antoine <aferedye@gmail.com>
2020-06-01 14:43:53 +0200

    New Entity Candidature
    Entité gérant à quelle date on postule et défini la date de relance

---
+++ src/Migrations/Version20200601122427.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200601122427 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE candidature (id INT AUTO_INCREMENT NOT NULL, society_id INT NOT NULL, nouvellecandidature DATETIME NOT NULL, relance DATETIME NOT NULL, UNIQUE INDEX UNIQ_E33BD3B8E6389D24 (society_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE candidature ADD CONSTRAINT FK_E33BD3B8E6389D24 FOREIGN KEY (society_id) REFERENCES society (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE candidature');
+    }
+}
